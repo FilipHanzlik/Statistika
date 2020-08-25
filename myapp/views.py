b008dd7876807791e04ef5e4db449322e58d23f2
Filip Viktor Hanzlik <filip.viktor@outlook.com>
2020-08-25 19:40:25 +0200

    Added new form for mobiles

--- myapp/views.py
+++ myapp/views.py
@@ -51,7 +51,7 @@ def evaluate(request):
 def results(request):
     came_from = request.META['HTTP_REFERER'].split('/')[-1]
     print(came_from)
-    if came_from[:5] == 'form':
+    if came_from[:4] == 'form':
         type = came_from[-1]
     else:
         type = request.GET['t']
